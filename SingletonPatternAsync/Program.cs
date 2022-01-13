387dd142d42612104602de38e910bfb7f16025e1
M F M Fazrin <mfmfazrin1986@gmail.com>
2022-01-13 12:47:10 +0530

    Add project files.

---
+++ SingletonPatternAsync/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+var fooObject = await Foo.CreateAsync();
+
+
+public sealed class Foo
+{
+    private Foo()
+    {
+    }
+
+    private async Task<Foo> InitAsync()
+    {
+        await Task.Delay(1000);
+        return this;
+    }
+
+    public static Task<Foo> CreateAsync()
+    {
+        var result = new Foo();
+        return result.InitAsync();
+    }
+}
